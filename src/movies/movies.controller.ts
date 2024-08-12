75f3fd8edd49c2571cbaff81a938f9fcd41ccbaf
YunDaeHyeon <kayuaao96@gmail.com>
2024-08-12 15:53:30 +0900

    feat: 데코레이터 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,29 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
+  /*
+    주의 : GET 처리를 할 때 라우트 파라미터 (:id)와 같은 것을 매핑한 함수가
+    상단에 있을 시 나머지 GET 처리는 동작하지 않는다.
+  */
   @Get()
   getAll() {
     return '모든 영화를 반환합니다.';
+  }
+
+  // @Query()에 포함되는 파라미터는 쿼리스트링 변수를 의미
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `${searchingYear}년도에 제작된 영화를 검색중입니다.`;
   }
 
   @Get('/:id')
@@ -13,8 +32,8 @@ export class MoviesController {
   }
 
   @Post()
-  create() {
-    return '영화를 생성하였습니다.';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,7 +42,18 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  path(@Param('id') moveId: string) {
-    return `아이다가 ${moveId}인 영화를 수정하였습니다.`;
+  path(@Param('id') moveId: string, @Body() updateData) {
+    // 업데이트할 영화의 id와 클라이언트로 넘어온 오브젝트 반환
+    return {
+      updateMovie: moveId,
+      ...updateData,
+    };
   }
 }
+
+/*
+@Param 데코레이터 : URL에 존재하는 Variable를 가져옴
+@Query 데코레이터 : Query 파라미터를 가져옴
+ex) @Param -> localhost:3000/movies/123 실행 시 123 가져옴
+ex) @Query -> locaohost:3000/movies/search?year=123 실행 시 123 가져옴
+*/
