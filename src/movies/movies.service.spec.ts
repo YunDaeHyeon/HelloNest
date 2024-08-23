119bf2bfe06bb077a1e445177f9cd37bc95801a5
YunDaeHyeon <kayuaao96@gmail.com>
2024-08-23 17:31:38 +0900

    feat: getAll, getOne 유닛 테스트 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 테스트 시작 전 실행
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +14,38 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // it(개별 테스트, Individual test)
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('배열을 반환하는가?', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('영화를 정상적으로 반환하는가?', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('404 에러를 출력하는가?', () => {
+      try {
+        service.getOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(
+          `[Error]ID가 999인 영화를 찾을 수 없습니다.`,
+        );
+      }
+    });
   });
 });
