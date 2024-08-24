a4236c12df94750cb04d42dbec59c0118f13238a
YunDaeHyeon <kayuaao96@gmail.com>
2024-08-24 17:43:29 +0900

    feat: GET Request e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach 대신 beforeAll을 사용하여 모든 테스트마다 테스트 모듈이 재생성되는 것을 막는다.
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    // 테스팅 모듈 생성
     app = moduleFixture.createNestApplication();
+    // 테스팅 모듈 또한 실제 서비스 모듈과 같이 동일한 설정을 해야한다. (Pipe 적용)
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +29,41 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  // /movie API test
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          genres: ['test'],
+          year: 2000,
+        })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  // route test
+  describe('/movies/:id', () => {
+    // todo()는 아직 구현되지 않은 테스트를 정의한다. (계획을 세워놓는다는 느낌)
+    // it.todo('GET');
+    // it.todo('DELETE');
+    // it.todo('PATCH');
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
   });
 });
